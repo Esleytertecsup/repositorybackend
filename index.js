2ebf88cece3440c32f37d5402c5eb8c4b9c290aa
Esleytertecsup <167911448+Esleytertecsup@users.noreply.github.com>
2025-10-15 22:17:57 -0500

    fix: server host for Render deployment

--- index.js
+++ index.js
@@ -1,18 +1,14 @@
 require('dotenv').config()
 const http = require('http')
 
-// Esta función ahora responderá al navegador
-function requestController(req, res){
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Bienvenidos al curso'); 
-    console.log('Petición recibida')
+function requestController(){
+    console.log('Bienvenidos al curso')
 }
 
 const server=http.createServer(requestController)
 
-const PORT=process.env.PORT || 10000 // Render te dará un puerto, 10000 es para pruebas locales
+const PORT=process.env.PORT
 
-// Aquí está el cambio clave: añadimos '0.0.0.0'
-server.listen(PORT, '0.0.0.0', function(){
-    console.log("Aplicacion corriendo en el puerto: " + PORT)
+server.listen(PORT, function(){
+    console.log("Aplicacion corriendo en: " + PORT)
 })
